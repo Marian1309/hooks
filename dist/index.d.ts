3efc6b9523ca0b5a2f2afd703f0447df9bbe9965
Marian <pidchashymaryan@gmail.com>
2025-06-19 13:22:30 +0300

    Update package.json with additional metadata, remove obsolete dist files, and ensure proper structure for hooks library.

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import useArray from "./hooks/use-array";
-import useDebounce from "./hooks/use-debounce";
-import useDebouncedCallback from "./hooks/use-debounced-callback";
-import useIsMounted from "./hooks/use-is-mounted";
-import useLocalStorage from "./hooks/use-local-storage";
-import usePreviousValue from "./hooks/use-previous-value";
-import useToggle from "./hooks/use-toggle";
-export { useArray, useDebounce, useDebouncedCallback, useIsMounted, useLocalStorage, usePreviousValue, useToggle, };
