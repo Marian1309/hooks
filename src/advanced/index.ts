d3d2e3252c615dd228acfe96b2b759da260dc669
Marian <pidchashymaryan@gmail.com>
2025-06-29 14:49:59 +0300

    Upgrade version to 3.0.7 in package.json, remove useAdvancedSearch hook from exports, and delete its implementation for codebase simplification.

--- src/advanced/index.ts
+++ src/advanced/index.ts
@@ -1,6 +1,5 @@
 import useFetch from "./hooks/use-fetch";
-import useAdvancedSearch from "./hooks/use-advanced-search";
 import useWhyDidUpdate from "./hooks/use-why-did-update";
 import useOptimisticUpdate from "./hooks/use-optimistic-update";
 
-export { useAdvancedSearch, useFetch, useOptimisticUpdate, useWhyDidUpdate };
+export { useFetch, useOptimisticUpdate, useWhyDidUpdate };
